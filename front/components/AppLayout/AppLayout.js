55297a4c89289873711f152ee8c30073c7869c96
log91 <peter1636@naver.com>
2020-02-27 18:03:38 +0900

    add: propTypes

--- front/components/AppLayout/AppLayout.js
+++ front/components/AppLayout/AppLayout.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Menu, Input } from 'antd';
+import Link from 'next/link';
+import PropTypes from 'prop-types';
 
 const AppLayout = ({ children }) => {
   return (
     <div>
       <Menu mode="horizontal">
-        <Menu.Item>Hello</Menu.Item>
-        <Menu.Item>World</Menu.Item>
+        <Menu.Item><Link href="/"><a>노드버드</a></Link></Menu.Item>
+        <Menu.Item><Link href="/profile"><a>Profile</a></Link></Menu.Item>
         <Menu.Item>
           <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
         </Menu.Item>
       </Menu>
+      <Link href="/signup"><button>회원가입</button></Link>
       {children}
     </div>
   )
 };
+
+AppLayout.propTypes = {
+  children: PropTypes.node
+}
 
 export default AppLayout;
